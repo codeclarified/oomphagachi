51e2abe2539e4f42c2f3519b1f9ed51a9c2feadf
Jason LeVan <jlevan@oomphinc.com>
2016-06-17 16:51:59 -0400

    Trying to fix stupid

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@ $(function() {
 				$.ajax(form_action, {
 					method: 'POST',
 					data: {
-						action: pet_action
+						petaction: pet_action
 					},
 					success: function(response, status) {
 						game.changeState(game_action);
