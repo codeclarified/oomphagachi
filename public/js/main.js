9f4aac067ddcd8d2a184cb11236a6328f28c1a99
Jason LeVan <jlevan@oomphinc.com>
2016-06-17 13:52:27 -0400

    Add JS button handling

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
 $(function() {
-
+	$("button[name='action']").click(function(e) {
+		e.preventDefault();
+		var form_action = $(this).parent('form').attr('action');
+		switch($(this).val()){
+			case 'panic':
+				alert('Panic jerks!');
+				break;
+			default:
+				$.ajax(form_action, {
+					method: 'POST',
+					data: {
+						action: form_action
+					},
+					success: function(response, status) {
+						console.log(response);
+					}
+				});
+				break;
+		}
+	});
 });
